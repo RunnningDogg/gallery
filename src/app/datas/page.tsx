daf14a9795b1813aefbb53802efe52cb6263115e
liangjiongxin <liangjiongxin@gmail.com>
2023-12-12 03:54:36 +0800

    fix layout

--- src/app/datas/page.tsx
+++ src/app/datas/page.tsx
@@ -6,15 +6,15 @@ type Props = {};
 
 export default function DataPage({}: Props) {
   return (
-    <div>
-      <h2 className=" scroll-m-20  pb-2 pl-2 text-3xl font-semibold tracking-tight first:mt-0">
+    <div className="ml-6">
+      <h2 className=" scroll-m-20  pb-2  text-3xl font-semibold tracking-tight first:mt-0">
         Data Page
       </h2>
       {/* <div className="mx-auto max-w-3xl rounded border">
         <Tiptap />
       </div> */}
 
-      <div className="rounder mx-auto max-w-3xl">
+      <div className="rounder  max-w-3xl">
         <CardForm />
       </div>
     </div>
