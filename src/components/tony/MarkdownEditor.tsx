98ef55ee4cdbd9159670401e9e1651d0fce7655e
liangjiongxin <liangjiongxin@gmail.com>
2023-12-14 03:26:30 +0800

    feature: del MarkdownEditor

--- src/components/tony/MarkdownEditor.tsx
+++ src/components/tony/MarkdownEditor.tsx
@@ -1,61 +1,69 @@
-"use client";
-import { $getRoot, $getSelection } from "lexical";
-import { useEffect, useState } from "react";
+// "use client";
+// import { $getRoot, $getSelection } from "lexical";
+// import { useEffect, useState } from "react";
 
-import { LexicalComposer } from "@lexical/react/LexicalComposer";
-import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
-import { ContentEditable } from "@lexical/react/LexicalContentEditable";
-import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+// import { LexicalComposer } from "@lexical/react/LexicalComposer";
+// import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
+// import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+// import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
 
-import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
-import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+// import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+// import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
 
-const theme = {
-  // Theme styling goes here
-  //...
-};
+// const theme = {
+//   // Theme styling goes here
+//   //...
+// };
+// type Props = {};
+
+// // Lexical React plugins are React components, which makes them
+// // highly composable. Furthermore, you can lazy load plugins if
+// // desired, so you don't pay the cost for plugins until you
+// // actually use them.
+// function MyCustomAutoFocusPlugin() {
+//   const [editor] = useLexicalComposerContext();
+
+//   useEffect(() => {
+//     // Focus the editor when the effect fires!
+//     editor.focus();
+//   }, [editor]);
+
+//   return null;
+// }
+
+// // Catch any errors that occur during Lexical updates and log them
+// // or throw them as needed. If you don't throw them, Lexical will
+// // try to recover gracefully without losing user data.
+// function onError(error) {
+//   console.error(error);
+// }
+
+// function Editor() {
+//   const initialConfig = {
+//     namespace: "MyEditor",
+//     theme,
+//     onError,
+//   };
+
+//   return (
+//     <LexicalComposer initialConfig={initialConfig}>
+//       <PlainTextPlugin
+//         contentEditable={<ContentEditable />}
+//         placeholder={<div>Enter some text...</div>}
+//         ErrorBoundary={LexicalErrorBoundary}
+//       />
+//       <HistoryPlugin />
+//       <MyCustomAutoFocusPlugin />
+//     </LexicalComposer>
+//   );
+// }
+
+// export default Editor;
+
+import React from "react";
+
 type Props = {};
 
-// Lexical React plugins are React components, which makes them
-// highly composable. Furthermore, you can lazy load plugins if
-// desired, so you don't pay the cost for plugins until you
-// actually use them.
-function MyCustomAutoFocusPlugin() {
-  const [editor] = useLexicalComposerContext();
-
-  useEffect(() => {
-    // Focus the editor when the effect fires!
-    editor.focus();
-  }, [editor]);
-
-  return null;
+export default function MarkdownEditor({}: Props) {
+  return <div>MarkdownEditor</div>;
 }
-
-// Catch any errors that occur during Lexical updates and log them
-// or throw them as needed. If you don't throw them, Lexical will
-// try to recover gracefully without losing user data.
-function onError(error) {
-  console.error(error);
-}
-
-function Editor() {
-  const initialConfig = {
-    namespace: "MyEditor",
-    theme,
-    onError,
-  };
-
-  return (
-    <LexicalComposer initialConfig={initialConfig}>
-      <PlainTextPlugin
-        contentEditable={<ContentEditable />}
-        placeholder={<div>Enter some text...</div>}
-        ErrorBoundary={LexicalErrorBoundary}
-      />
-      <HistoryPlugin />
-      <MyCustomAutoFocusPlugin />
-    </LexicalComposer>
-  );
-}
-
-export default Editor;
