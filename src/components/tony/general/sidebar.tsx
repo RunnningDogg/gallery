20f218c2da2eb23cfabcfd6e0d154688dd1de5da
liangjiongxin <liangjiongxin@gmail.com>
2023-12-17 15:38:03 +0800

    feature: optimize sidebar, add sidebar fold

--- src/components/tony/general/sidebar.tsx
+++ src/components/tony/general/sidebar.tsx
@@ -1,10 +1,18 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import { AlbumIcon, CpuIcon, PenSquare, Table, Table2 } from "lucide-react";
+
+import {
+  AlbumIcon,
+  ArrowLeftFromLine,
+  CpuIcon,
+  MenuIcon,
+  PenSquare,
+  Table,
+  Table2,
+} from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useRouter } from "next/router";
 import React, { useState } from "react";
 
 type Props = {};
@@ -12,10 +20,88 @@ type Props = {};
 export default function Sidebar({}: Props) {
   const pathName = usePathname();
 
-  // const router = useRouter();
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  // sidebar items
+  const sidebarNavItems = [
+    {
+      title: "Read",
+      icon: (
+        <AlbumIcon
+          className={cn("stroke-1", {
+            " rounded bg-slate-100 text-indigo-500": pathName === "/read",
+            "h-[40px] w-[40px] p-2": !isSidebarOpen,
+          })}
+        />
+      ),
+      href: "/read",
+    },
+    {
+      title: "Memorise",
+      icon: (
+        <CpuIcon
+          className={cn("stroke-1", {
+            "rounded bg-slate-100 text-indigo-500": pathName === "/cards",
+            "h-[40px] w-[40px] p-2": !isSidebarOpen,
+          })}
+        />
+      ),
+      href: "/cards",
+    },
+    {
+      title: "Add a Note",
+      icon: (
+        <PenSquare
+          className={cn("stroke-1", {
+            "rounded bg-slate-100 text-indigo-500": pathName === "/notes",
+            "h-[40px] w-[40px] p-2": !isSidebarOpen,
+          })}
+        />
+      ),
+      href: "/notes",
+    },
+    {
+      title: "Data",
+      icon: (
+        <Table2
+          className={cn("stroke-1", {
+            "rounded bg-slate-100 text-indigo-500": pathName === "/table",
+            "h-[40px] w-[40px] p-2": !isSidebarOpen,
+          })}
+        />
+      ),
+      href: "/table",
+    },
+  ];
+
   return (
-    <aside className="flex h-screen  flex-col items-center border-r border-slate-200">
-      <div className="flex flex-col space-y-2 border-b px-4 py-4 ">
+    <aside
+      className={cn(
+        " relative flex h-screen  flex-col items-center border-r border-slate-200 transition-all duration-150",
+        {
+          "w-60": isSidebarOpen,
+          "w-20": !isSidebarOpen,
+        },
+      )}
+    >
+      {/* 当折叠的时候, 得有一个按钮用来展开 */}
+      {!isSidebarOpen && (
+        <Button
+          variant={"outline"}
+          className="absolute -right-16 top-2"
+          onClick={() => setIsSidebarOpen(true)}
+        >
+          <MenuIcon />
+        </Button>
+      )}
+
+      {/* logo */}
+      <div
+        className={cn("flex flex-col space-y-2 border-b", {
+          "p-4": isSidebarOpen,
+          "p-2": !isSidebarOpen,
+        })}
+      >
         <Link
           href="/"
           className=" title-font flex cursor-pointer items-center   font-medium text-gray-900"
@@ -32,118 +118,57 @@ export default function Sidebar({}: Props) {
           >
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
           </svg>
-          <span className="ml-3 text-xl">NoteWise</span>
+          {isSidebarOpen && <span className="ml-3 text-xl">NoteWise</span>}
         </Link>
       </div>
 
-      <div className="mt-3 w-full px-4">
+      {/* nav */}
+      <div
+        className={cn("mt-3 flex h-full w-full flex-col", {
+          "px-4": isSidebarOpen,
+        })}
+      >
         {/* <Button className="w-full">Add Note</Button> */}
-        <ul className="mt-6 w-full space-y-3">
-          <li>
-            <Link
-              href="/read"
-              className={cn(
-                "flex cursor-pointer gap-1 rounded p-2 transition-colors duration-150 hover:bg-slate-100  ",
-                {
-                  "bg-slate-100 ": pathName === "/read",
-                },
-              )}
-            >
-              <AlbumIcon
-                className={cn("stroke-1", {
-                  "text-indigo-500": pathName === "/read",
-                })}
-              />
-
-              <span
-                className={cn({
-                  "bg-gradient-to-r from-violet-500 to-indigo-400 bg-clip-text font-bold text-transparent":
-                    pathName === "/read",
-                })}
+        <ul className="mt-6 w-full flex-1 space-y-3">
+          {sidebarNavItems.map((item) => (
+            <li key={item.title}>
+              <Link
+                href={item.href}
+                className={cn(
+                  "flex cursor-pointer  rounded  transition-colors duration-150   ",
+                  {
+                    "bg-slate-100 hover:bg-slate-100":
+                      pathName === item.href && isSidebarOpen,
+                    "gap-2 p-2": isSidebarOpen,
+                    "justify-center p-1": !isSidebarOpen,
+                  },
+                )}
               >
-                Read
-              </span>
-            </Link>
-          </li>
-
-          <li>
-            <Link
-              href="/cards"
-              className={cn(
-                "flex cursor-pointer gap-1 rounded p-2 transition-colors duration-150 hover:bg-slate-100  ",
-                {
-                  "bg-slate-100 ": pathName === "/cards",
-                },
-              )}
-            >
-              <CpuIcon
-                className={cn("stroke-1", {
-                  "text-indigo-500": pathName === "/cards",
-                })}
-              />
-              <span
-                className={cn({
-                  "bg-gradient-to-r from-violet-500 to-indigo-400 bg-clip-text font-bold text-transparent":
-                    pathName === "/cards",
-                })}
-              >
-                Memorise
-              </span>
-            </Link>
-          </li>
-
-          <li>
-            <Link
-              href="/notes"
-              className={cn(
-                "flex cursor-pointer gap-1 rounded p-2 transition-colors duration-150 hover:bg-slate-100  ",
-                {
-                  "bg-slate-100": pathName === "/notes",
-                },
-              )}
-            >
-              <PenSquare
-                className={cn("stroke-1", {
-                  "text-indigo-500": pathName === "/notes",
-                })}
-              />
-              <span
-                className={cn({
-                  "bg-gradient-to-r from-violet-500 to-indigo-400 bg-clip-text font-bold text-transparent":
-                    pathName === "/notes",
-                })}
-              >
-                Add a Note
-              </span>
-            </Link>
-          </li>
-
-          <li>
-            <Link
-              href="/table"
-              className={cn(
-                "flex cursor-pointer gap-1 rounded p-2 transition-colors duration-150 hover:bg-slate-100  ",
-                {
-                  "bg-slate-100": pathName === "/table",
-                },
-              )}
-            >
-              <Table2
-                className={cn("stroke-1", {
-                  "text-indigo-500": pathName === "/table",
-                })}
-              />
-              <span
-                className={cn({
-                  "bg-gradient-to-r from-violet-500 to-indigo-400 bg-clip-text font-bold text-transparent":
-                    pathName === "/table",
-                })}
-              >
-                Data
-              </span>
-            </Link>
-          </li>
+                {item.icon}
+                {isSidebarOpen && (
+                  <span
+                    className={cn({
+                      "bg-gradient-to-r from-violet-500 to-indigo-400 bg-clip-text font-bold text-transparent":
+                        pathName === item.href,
+                    })}
+                  >
+                    {item.title}
+                  </span>
+                )}
+              </Link>
+            </li>
+          ))}
         </ul>
+
+        {isSidebarOpen && (
+          <Button
+            variant="outline"
+            className=" mb-3"
+            onClick={() => setIsSidebarOpen(false)}
+          >
+            <ArrowLeftFromLine />
+          </Button>
+        )}
       </div>
     </aside>
   );
